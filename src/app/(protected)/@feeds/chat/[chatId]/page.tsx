1b9ae3979becfe03f80f1479e7b5451f0d9a8401
Ruslan Volovenko <vokoloven@gmail.com>
2026-05-25 19:20:07 +0300

    fix(proxy.ts): improvements

--- src/app/(protected)/@feeds/chat/[chatId]/page.tsx
+++ src/app/(protected)/@feeds/chat/[chatId]/page.tsx
@@ -1,18 +1,15 @@
-import { chatIdPageParamsSchema } from './schemas/page.schema';
-import type { TZodInfer } from '@/core/models/utility.model';
+import type { TParams } from '@/core/models/params.model';
 
-const generateMetadata = async ({ params }: TZodInfer<typeof chatIdPageParamsSchema>) => {
-  const { chatId } = await chatIdPageParamsSchema.shape.params.parseAsync(params);
-
+const generateMetadata = async ({ params }: TParams<'chatId'>) => {
+  const { chatId } = await params;
   return {
     title: `${chatId} | Chat`,
     description: 'Chat name'
   };
 };
 
-const ChatPage = async ({ params }: TZodInfer<typeof chatIdPageParamsSchema>) => {
-  const { chatId } = await chatIdPageParamsSchema.shape.params.parseAsync(params);
-
+const ChatPage = async ({ params }: TParams<'chatId'>) => {
+  const { chatId } = await params;
   return <h1>ChatPage {chatId} is created</h1>;
 };
 
--- src/app/(protected)/@feeds/chat/[chatId]/schemas/page.schema.ts
+++
@@ -1,7 +0,0 @@
-import z from 'zod';
-
-const chatIdPageParamsSchema = z.object({
-  params: z.promise(z.record(z.literal('chatId'), z.string()))
-});
-
-export { chatIdPageParamsSchema };
