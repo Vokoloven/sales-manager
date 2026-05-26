fc2214a9d50d7c5b205ee71f111744b2c9f7bdd6
Ruslan Volovenko <vokoloven@gmail.com>
2026-05-26 13:08:46 +0300

    feat(table): optimized Progress component

--- src/components/Progress/Progress.tsx
+++ src/components/Progress/Progress.tsx
@@ -1,20 +1,12 @@
-'use client';
-
-import classnames from 'classnames';
-import { useProgress } from './hooks/useProgress';
 import type { TProgressProps } from './models/progress.model';
 import styles from './Progress.module.css';
 
-const Progress = ({ value }: TProgressProps) => {
-  const { progress } = useProgress();
-
-  return (
-    <progress
-      className={classnames('Progress', styles.progress)}
-      max={100}
-      value={value ?? progress}
-    />
-  );
-};
+const Progress = ({ value }: TProgressProps) => (
+  <progress
+    className={`Progress ${styles.progress}${value === undefined ? ` ${styles.progressAnimated}` : ''}`}
+    max={100}
+    value={value ?? 100}
+  />
+);
 
 export default Progress;
