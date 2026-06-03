f89aba33854cfed396ca725d7b7c0b04d5cf9bf0
Ruslan Volovenko <vokoloven@gmail.com>
2026-06-03 13:00:30 +0300

    feat(chat): added buttons
    - Added tag constant

--- src/app/(protected)/@aside/actions/chat.action.ts
+++ src/app/(protected)/@aside/actions/chat.action.ts
@@ -3,6 +3,7 @@
 import { cacheTag, revalidateTag } from 'next/cache';
 import { API_URL } from '@/core/constants/apiURL.constant';
 import { apiService } from '@/core/services/ApiService.service';
+import { CHAT_TAG } from '../constants/chat.constant';
 import { chatService } from '../services/Chat.service';
 import type { chatRequestSchema, chatsResponseSchema } from '../schemas/chat.schema';
 import type { TToken } from '@/core/models/token.model';
@@ -10,18 +11,18 @@ import type { TZodInfer } from '@/core/models/utility.model';
 
 const getChatsAction = async (accessToken: TToken['accessToken']) => {
   'use cache';
-  cacheTag('chats');
+  cacheTag(CHAT_TAG.chats);
   return apiService(accessToken).api<TZodInfer<typeof chatsResponseSchema>>(API_URL.chats);
 };
 
 const renameChatAction = async (params: TZodInfer<typeof chatRequestSchema> & { id: string }) => {
   await chatService.renameChat(params);
-  revalidateTag('chats', { expire: 0 });
+  revalidateTag(CHAT_TAG.chats, { expire: 0 });
 };
 
 const deleteChatAction = async (id: string) => {
   await chatService.deleteChat(id);
-  revalidateTag('chats', { expire: 0 });
+  revalidateTag(CHAT_TAG.chats, { expire: 0 });
 };
 
 export { getChatsAction, renameChatAction, deleteChatAction };
---
+++ src/app/(protected)/@aside/constants/chat.constant.ts
@@ -0,0 +1,6 @@
+const CHAT_TAG = {
+  chat: 'chat',
+  chats: 'chats'
+} as const;
+
+export { CHAT_TAG };
