6a30297c47592d14393430e6ab38230676381ac5
Ruslan Volovenko <vokoloven@gmail.com>
2026-06-03 15:08:34 +0300

    fix(FeedsLayout): moved server component header

--- src/app/(protected)/@feeds/layout.tsx
+++ src/app/(protected)/@feeds/layout.tsx
@@ -5,7 +5,9 @@ import styles from './layout.module.css';
 const FeedsLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className={styles.root}>
-      <FeedsHeader />
+      <header className={styles.header}>
+        <FeedsHeader />
+      </header>
       <div className={styles.content}>{children}</div>
     </div>
   );
